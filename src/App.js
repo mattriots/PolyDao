40e2313af9c654d6b52114982a21ccc5a6ef641a
mattriots <mattdepauw@gmail.com>
2022-04-08 21:49:40 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { InitSwAuth } from '@skill-wallet/auth';
 import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    InitSwAuth();
+}, []);
+
   return (
     <div>
         <sw-auth partner-key="d598a1d4a3688ef091006511966dfae96961b60f" use-dev='true'></sw-auth>
@@ -10,7 +14,6 @@ function App() {
   );
 }
 
-InitSwAuth();
 export default App;
 
 // return (
