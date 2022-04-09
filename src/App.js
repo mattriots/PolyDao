25243175969ed57d025b1e7fb49cab2584b73f64
Howl1935 <travisphawley@gmail.com>
2022-04-09 14:55:23 -0700

    functioning properly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { useRef } from 'react';
 
-import Navbar
- from './components/Navbar';
-import Home from './pages/Home'
+import Navbar from './components/Navbar';
+
+import Section0 from './components/Section0';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
+import Section4 from './components/Section4';
+import Section5 from './components/Section5';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
- 
-  return (
-		<div className="container mx-auto bg-[#121E2A]">
-			<Navbar />
+	const section0 = useRef(null);
+	const section1 = useRef(null);
+	const section2 = useRef(null);
+	const section3 = useRef(null);
+	const section4 = useRef(null);
+	const section5 = useRef(null);
 
-			<Routes>
-				{ <Route path="/" element={<Home />} />
-				/*<Route path="about" element={<About />} /> */}
-			</Routes>
+	const scrollToSection = (elementRef) => {
+		window.scrollTo({
+			top: elementRef.current.offsetTop,
+			behavior: 'smooth',
+		});
+	};
+
+	return (
+		<div className="container mx-auto bg-[#121E2A]">
+			<ScrollToTop />
+			<Navbar
+				scrollToSection={scrollToSection}
+				section0={section0}
+				section1={section1}
+				section2={section2}
+				section3={section3}
+				section4={section4}
+				section5={section5}
+			/>
+			<div>
+				<div ref={section0} className="flex justify-center">
+					<Section0 />
+				</div>
+				<div ref={section1} className="flex justify-center">
+					<Section1 />
+				</div>
+				<div ref={section2} className="flex justify-center">
+					<Section2 />
+				</div>
+				<div ref={section3} className="flex justify-center">
+					<Section3 />
+				</div>
+				<div ref={section4} className="flex justify-center">
+					<Section4 />
+				</div>
+				<div ref={section5} className="flex justify-center">
+					<Section5 />
+				</div>
+			</div>
 		</div>
 	);
 }
 
 export default App;
-
-
