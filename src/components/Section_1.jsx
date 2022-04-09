5ee8a97bb776458cf1a3bd64147deb9b59be4668
Matt <mattdepauw@gmail.com>
2022-04-09 13:37:39 -0700

    started sections

---
+++ src/components/Section_1.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import sec1 from "./assets/Section_1.png";
+
+
+function Section_1(){
+    return (<div>
+        <img
+            className=''
+            src={sec1}
+            alt="section1"
+        ></img>
+    </div>
+
+    );
+}
+export default Section_1;
