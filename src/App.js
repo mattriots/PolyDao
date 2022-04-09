d2064543a897bd385faa4fa5e880e93ac329a0cc
Howl1935 <travisphawley@gmail.com>
2022-04-09 13:40:55 -0700

    starting to style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-import { Routes, Route, Link } from 'react-router-dom';
 import Navbar
  from './components/Navbar';
 function App() {
@@ -8,10 +8,9 @@ function App() {
   return (
 		<div className="container mx-auto bg-[#121E2A]">
 			<Navbar />
-			<Routes>
-				{/* <Route path="/" element={<Home />} />
-				<Route path="about" element={<About />} /> */}
-			</Routes>
+
+      
+		
 		</div>
 	);
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { InitSwAuth } from '@skill-wallet/auth';
 import { useEffect } from 'react';
-
+import logo from '../assets/cp-logo.png'
   
 
 function Navbar() {
@@ -11,8 +11,15 @@ function Navbar() {
 }, []);
   return (
 		<>
-        
-			<div>
+			<div className="flex flex-row justify-between items-center bg-[#172231] h-20">
+				<div className="flex flex-row text-white">
+					<img src={logo} className='h-20' alt="this is a cal poly logo" />
+					<ul className="flex flex-row items-center justify-center text-white">
+						<li>one</li>
+						<li>two</li>
+						<li>three</li>
+					</ul>
+				</div>
 				<sw-auth
 					partner-key="d598a1d4a3688ef091006511966dfae96961b60f"
 					use-dev="true"
